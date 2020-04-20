bdb70797c08e7f703b22f8b00718c8e211c210f7
Abigail McHugh <amchugh2@binghamton.edu>
2020-04-19 20:38:17 -0400

    set associative working

--- project2/cache-sim.cc
+++ project2/cache-sim.cc
@@ -16,7 +16,7 @@ struct entry {
 };
 
 // cache entry struct
-struct cache {
+struct Cache {
 	int valid;
 	unsigned long tag;
 	unsigned long index;
@@ -26,9 +26,9 @@ struct cache {
 int directMapped(vector<entry> entries, int cacheSize){
 	int hits = 0;
 	// assume each cache line size is 32 bytes
-	unsigned long long cacheLines = cacheSize / 32;
+	unsigned long cacheLines = cacheSize / 32;
 	// create cache structure
-	struct cache cache[cacheLines];
+	struct Cache cache[cacheLines];
 
 	// initialize cache
 	for(int i = 0; i < cacheLines; i++){
@@ -55,14 +55,15 @@ int directMapped(vector<entry> entries, int cacheSize){
 	return hits;
 }
 
-int setAssociative(vector<entry> entries, int assoc) {
+int setAssociative(vector<entry> entries, int sets) {
 	int hits = 0;
-	unsigned long cacheLines = 512/assoc;
-
-	// create and initialize cache
-	struct cache[cacheLines][associativity];
-	for(int i = 0; i < cacheLines; i++){
-		for(int j = 0; j < assoc; j++){
+	
+	unsigned long cacheLines = 512/sets;
+	struct Cache cache[cacheLines][sets];
+	
+	//initialize values
+	for (int i = 0; i < cacheLines; i++){
+		for (int j = 0; j < sets; j++){
 			cache[i][j].valid = 0;
 			cache[i][j].tag = 0;
 			cache[i][j].index = 0;
@@ -70,56 +71,50 @@ int setAssociative(vector<entry> entries, int assoc) {
 		}
 	}
 
-	// loop through entries
-	for(entry e : entries){
-		// offset by 5
-		unsigned long long tag = e.address >> 5;
+	//check for hits
+	for (entry e: entries){
+		unsigned long tag = e.addr >> 5;
 		unsigned long index = tag % cacheLines;
-		// loop thru sets
-		for(int i = 0; i < assoc; i++){
-			if(cache[index][i].valid == 1 && cache[index][i].tag = tag){
-				hits+=1;
-				// set as most recently used
-				for(int j = 0; i < assoc; j++){
-					// if not most recently used
-					if(j != 1){
-						cache[index][j].lru = 1;
-					}
-					break;
+
+		for (int i = 0; i < sets; i++){
+			if (cache[index][i].valid && cache[index][i].tag == tag){
+				//cache hit!!
+				hits++;
+				for (int j = 0; j < sets; j++){
+					if (j != i) cache[index][j].lru = cache[index][j].lru + 1;
+					else cache[index][j].lru = 0;
 				}
-				// if valid bit is 0
-				else if(cache[index][i].valid == 0){
-					// insert
-					cache[index][i].tag = tag;
-					cache[index][i].valid = 1;
-					// set as most recently used
-					for(int j = 0; j < assoc; j++){
-						if(j != 1){
-							cache[index][j].lru = 1;
-						}
-					}
-				}
-				else if(i == (assoc - 1)){
-					int high = cache[index][i].lru;
-					int highIndex = i;
-					for(int j = 0; j < assoc; j++){
-						if(cache[index][j].lru > high){
-							high = cache[index][j].lru;
-							highIndex = j;
-						}
-					}
-					for(int j = 0; j < assoc; j++){
-						if( j!= highIndex){
-							cache[index][j].lru = 1;
-						}
-					}
-					cache[index][highIndex].tag = tag;
-					cache[index][highIndex].valid = 1;
-				} 
+				break;
+			}
+			else if (!cache[index][i].valid){
+				cache[index][i].tag = tag;
+				cache[index][i].valid = 1;
+				for (int j = 0; j < sets; j++){
+					if (j != i) cache[index][j].lru = cache[index][j].lru + 1;
+					else cache[index][j].lru = 0;
 				}
 			}
-		return hits;
+			else if (i == (sets-1)){
+				int high = cache[index][i].lru;
+				int highIndex = i;
+				for (int j = 0; j < sets; j++){
+					if (cache[index][j].lru > high){
+						high = cache[index][j].lru;
+						highIndex = j;
+					}
+				}
+				cache[index][highIndex].lru = 0;
+				for (int j = 0; j < sets; j++){
+					if (j != highIndex) cache[index][j].lru = cache[index][j].lru + 1;
+					else cache[index][j].lru = 0;
+				}
+				cache[index][highIndex].tag = tag;
+				cache[index][highIndex].valid = 1;
+			}
+		}
 	}
+	return hits;
+}
 
 int main(int argc, char *argv[]){
 	
